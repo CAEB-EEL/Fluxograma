2644d9f4af4f6dee793b417857595ae87b1bc67e
CAEB-EEL <assuntosacademicoscaeb@gmail.com>
2025-06-23 01:00:19 -0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,414 @@
+const data = {
+    "Leitura e Interpretação de Desenho Técnico": {
+        "periodo": 1,
+        "requisito": []
+    },
+    "Cálculo I": {
+        "periodo": 1,
+        "requisito": []       
+    },
+    "Fundamentos de Química para Engenharia I-B": {
+        "periodo": 1,
+        "requisito": []
+    },
+    "Biologia Celular": {
+        "periodo": 1,
+        "requisito": []
+    },
+    "Geometria Analítica": {
+        "periodo": 1,
+        "requisito": []
+    },
+    "Comunicação Científica em Biotecnologia": {
+        "periodo": 1,
+        "requisito": []
+    },
+    "Introdução à Engenharia Bioquímica": {
+        "periodo": 1,
+        "requisito": []
+    },
+    "Álgebra Linear": {
+        "periodo": 2,
+        "requisito": [
+            "Geometria Analítica"
+        ]
+    },
+    "Cálculo II": {
+        "periodo": 2,
+        "requisito": [
+            "Cálculo I",
+            "Geometria Analítica"
+        ]
+    },
+    "Física I": {
+        "periodo": 2,
+        "requisito": []
+    },
+    "Química Geral Experimental": {
+        "periodo": 2,
+        "requisito": [
+            "Fundamentos de Química para Engenharia I-B"
+        ]
+    },
+    "Fundamentos de Química para Engenharia II-A": {
+        "periodo": 2,
+        "requisito": [
+            "Fundamentos de Química para Engenharia I-B"
+        ]
+    },
+    "Microbiologia: da Teoria à Prática": {
+        "periodo": 2,
+        "requisito": [
+            "Biologia Celular"
+        ]
+    },
+    "Física Experimental I": {
+        "periodo": 2,
+        "requisito": []
+    },
+    "Introdução aos Métodos Numéricos e Computacionais": {
+        "periodo": 3,
+        "requisito": [
+            "Geometria Analítica",
+            "Cálculo II"
+        ]
+    },
+    "Balanços de Massa e Energia": {
+        "periodo": 3,
+        "requisito": []
+    },
+    "Cálculo III": {
+        "periodo": 3,
+        "requisito": [
+            "Cálculo II"
+        ]
+    },
+    "Mecânica": {
+        "periodo": 3,
+        "requisito": [
+            "Geometria Analítica",
+            "Física I"
+        ]
+    },
+    "Física II": {
+        "periodo": 3,
+        "requisito": [
+            "Cálculo I",
+            "Física I"
+        ]
+    },
+    "Química Orgânica Fundamental": {
+        "periodo": 3,
+        "requisito": [
+            "Fundamentos de Química para Engenharia II-A"
+        ]
+    },
+    "Química Bioinorgânica": {
+        "periodo": 3,
+        "requisito": [
+            "Fundamentos de Química para Engenharia II-A"
+        ]
+    },
+    "Cálculo IV": {
+        "periodo": 4,
+        "requisito": [
+            "Álgebra Linear",
+            "Cálculo II"
+        ]
+    },
+    "Física III": {
+        "periodo": 4,
+        "requisito": [
+            "Física II",
+            "Cálculo II"
+        ]
+    },
+    "Termodinâmica Química Aplicada I": {
+        "periodo": 4,
+        "requisito": [
+            "Cálculo II",
+            "Balanços de Massa e Energia",
+            "Física II"
+        ]
+    },
+    "Estrutura e Química de Materiais Lignocelulósicos": {
+        "periodo": 4,
+        "requisito": [
+            "Química Orgânica Fundamental"
+        ]
+    },
+    "Física Experimental III": {
+        "periodo": 4,
+        "requisito": [
+            "Física Experimental I"
+        ]
+    },
+    "Bioquímica I": {
+        "periodo": 4,
+        "requisito": [
+            "Química Orgânica Fundamental",
+            "Biologia Celular"
+        ]
+    },
+    "Bioquímica Experimental I": {
+        "periodo": 4,
+        "requisito": []
+    },
+    "Fenômenos de Transporte I": {
+        "periodo": 4,
+        "requisito": [
+            "Mecânica",
+            "Cálculo III"
+        ]
+    },
+    "Eletricidade Aplicada": {
+        "periodo": 5,
+        "requisito": [
+            "Física III"
+        ]
+    },
+    "Fenômenos de Transporte II": {
+        "periodo": 5,
+        "requisito": [
+            "Fenômenos de Transporte I",
+            "Cálculo IV",
+            "Física II"
+        ]
+    },
+    "Termodinâmica Química Aplicada II": {
+        "periodo": 5,
+        "requisito": [
+            "Termodinâmica Química Aplicada I"
+        ]
+    },
+    "Laboratório de Eletricidade": {
+        "periodo": 5,
+        "requisito": [
+            "Física Experimental III"
+        ]
+    },
+    "Tecnologia de Conversão de Biomassa Vegetal": {
+        "periodo": 5,
+        "requisito": [
+            "Estrutura e Química de Materiais Lignocelulósicos"
+        ]
+    },
+    "Bioquímica II": {
+        "periodo": 5,
+        "requisito": [
+            "Bioquímica I"
+        ]
+    },
+    "Bioquímica Experimental II": {
+        "periodo": 5,
+        "requisito": []
+    },
+    "Estatística": {
+        "periodo": 5,
+        "requisito": [
+            "Cálculo II"
+        ]
+    },
+    "Introdução à Ciência dos Materiais": {
+        "periodo": 5,
+        "requisito": []
+    },
+    "Operações Unitárias I": {
+        "periodo": 5
+        "requisito": [
+            "Fenômenos de Transporte I"
+        ]
+    },
+    "Análise Técnico-Econômica de Bioprocessos": {
+        "periodo": 6,
+        "requisito": [
+            "Balanços de Massa e Energia"
+        ]
+    },
+    "Fenômenos de Transporte III": {
+        "periodo": 6,
+        "requisito": [
+            "Fenômenos de Transporte II"
+        ]
+    },
+    "Tecnologia de Processos Fermentativos": {
+        "periodo": 6,
+        "requisito": [
+            "Bioquímica II",
+            "Microbiologia: da Teoria à Prática"
+        ]
+    },
+    "Química Analítica Aplicada a Bioprocessos": {
+        "periodo": 6,
+        "requisito": [
+            "Química Geral Experimental",
+            "Fundamentos de Química para Engenharia II-A",
+            "Bioquímica Experimental I"
+        ]
+    },
+    "Operações Unitárias II": {
+        "periodo": 6,
+        "requisito": [
+            "Fenômenos de Transporte II"
+        ]
+    },
+    "Engenharia de Segurança do Trabalho e Biossegurança": {
+        "periodo": 6,
+        "requisito": []
+    },
+    "Engenharia Genética Teórica e Prática": {
+        "periodo": 6,
+        "requisito": [
+            "Biologia Celular",
+            "Microbiologia: da Teoria à Prática"
+        ]
+    },
+    "Tecnologia de Biopolímeros": {
+        "periodo": 6,
+        "requisito": [
+            "Química Orgânica Fundamental"
+        ]
+    },
+    "Enzimologia": {
+        "periodo": 7,
+        "requisito": [
+            "Engenharia Genética Teórica e Prática",
+            "Bioquímica I",
+            "Microbiologia: da Teoria à Prática"
+        ]
+    },
+    "Operações Unitárias III": {
+        "periodo": 7,
+        "requisito": [
+            "Fenômenos de Transporte III"
+        ]
+    },
+    "Engenharia Bioquímica I": {
+        "periodo": 7,
+        "requisito": [
+            "Tecnologia de Processos Fermentativos"
+        ]
+    },
+    "Genética e Biotecnologia Vegetal": {
+        "periodo": 7,
+        "requisito": [
+            "Engenharia Genética Teórica e Prática",
+            "Bioquímica II",
+            "Microbiologia: da Teoria à Prática"
+        ]
+    },
+    "Introdução ao Gerenciamento de Projetos Ambientais": {
+        "periodo": 7,
+        "requisito": []
+    },
+    "Instrumentação e Controle de Bioprocessos": {
+        "periodo": 7,
+        "requisito": [
+            "Eletricidade Aplicada",
+            "Fenômenos de Transporte II"
+        ]
+    },
+    "Processos Bioquímicos Industriais": {
+        "periodo": 7,
+        "requisito": [
+            "Operações Unitárias II",
+            "Tecnologia de Processos Fermentativos"
+        ]
+    },
+    "Tratamento Biológico de Efluentes": {
+        "periodo": 7,
+        "requisito": [
+            "Microbiologia: da Teoria à Prática"
+        ]
+    },
+    "Empreendedorismo Tecnológico": {
+        "periodo": 7,
+        "requisito": [
+            "Análise Técnico-Econômica de Bioprocessos"
+        ]
+    },
+    "Modelagem e Simulação de Processos Biotecnológicos": {
+        "periodo": 8,
+        "requisito": [
+            "Introdução aos Métodos Numéricos e Computacionais",
+            "Operações Unitárias II"
+        ]
+    },
+    "Laboratório de Engenharia Química III": {
+        "periodo": 8,
+        "requisito": [
+            "Operações Unitárias I",
+            "Operações Unitárias II"
+        ]
+    },
+    "Laboratório de Engenharia Bioquímica": {
+        "periodo": 8,
+        "requisito": [
+            "Engenharia Bioquímica I",
+            "Enzimologia"
+        ]
+    },
+    "Reatores Bioquímicos": {
+        "periodo": 8,
+        "requisito": [
+            "Engenharia Bioquímica I"
+        ]
+    },
+    "Engenharia Bioquímica II": {
+        "periodo": 8,
+        "requisito": [
+            "Engenharia Bioquímica I"
+        ]
+    },
+    "Estatística em Bioprocessos": {
+        "periodo": 8,
+        "requisito": [
+            "Estatística",
+            "Processos Bioquímicos Industriais"
+        ]
+    },
+    "Solução de Problemas de Engenharia": {
+        "periodo": 8,
+        "requisito": [
+            "Tecnologia de Biopolímeros",
+            "Engenharia Bioquímica I",
+            "Enzimologia",
+            "Processos Bioquímicos Industriais",
+            "Tecnologia de Conversão de Biomassa Vegetal",
+            "Genética e Biotecnologia Vegetal"
+        ]
+    },
+       "Projeto Final de Curso em Engenharia Bioquímica": {
+        "periodo": 9,
+        "requisito": [
+            "Solução de Problemas de Engenharia",
+            "Introdução ao Gerenciamento de Projetos Ambientais"
+        ]
+    },
+    "Estágio Supervisionado": {
+        "periodo": 10,
+        "requisito": [
+            "Química Geral Experimental",
+            "Fundamentos de Química para Engenharia II-A",
+            "Fundamentos de Química para Engenharia I-B",
+            "Cálculo IV",
+            "Laboratório de Eletricidade",
+            "Física III",
+            "Introdução aos Métodos Numéricos e Computacionais",
+            "Cálculo I",
+            "Eletricidade Aplicada",
+            "Estatística",
+            "Mecânica",
+            "Geometria Analítica",
+            "Álgebra Linear",
+            "Física Experimental I",
+            "Física Experimental III",
+            "Cálculo III",
+            "Introdução à Ciência dos Materiais",
+            "Cálculo II",
+            "Leitura e Interpretação de Desenho Técnico",
+            "Física I",
+            "Física II"
+        ]
+    }
+}
