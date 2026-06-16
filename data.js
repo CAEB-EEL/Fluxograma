b47a63641bc9ac9eb4cb890552ee078ac010acd7
CAEB-EEL <assuntosacademicoscaeb@gmail.com>
2026-06-16 20:54:35 -0300

    Update data.js

--- data.js
+++ data.js
@@ -1,19 +1,58 @@
 const curriculos = {
     "2022": {
-        // Cole aqui os objetos das matérias do ano de 2022
+        // --- COLOQUE AS MATÉRIAS DE 2022 AQUI ---
+        "Cálculo I": {
+            "periodo": 1,
+            "requisito": []
+        },
+        "Geometria Analítica": {
+            "periodo": 1,
+            "requisito": []
+        },
+        "Cálculo II": {
+            "periodo": 2,
+            "requisito": ["Cálculo I"]
+        }
     },
+    
     "2023": {
-        // Cole aqui os objetos das matérias do ano de 2023
+        // --- COLOQUE AS MATÉRIAS DE 2023 AQUI ---
+        "Matéria Exemplo 1 - 2023": {
+            "periodo": 1,
+            "requisito": []
+        },
+        "Matéria Exemplo 2 - 2023": {
+            "periodo": 2,
+            "requisito": ["Matéria Exemplo 1 - 2023"]
+        }
     },
+    
     "2025": {
-        // Cole aqui os objetos das matérias do ano de 2025
+        // --- COLOQUE AS MATÉRIAS DE 2025 AQUI ---
+        "Matéria Exemplo 1 - 2025": {
+            "periodo": 1,
+            "requisito": []
+        }
     },
+    
     "2026": {
-        // Cole aqui os objetos do data.js atual que você me enviou
+        // --- COLOQUE AS MATÉRIAS DE 2026 AQUI ---
+        // (Você pode colar todo aquele seu código original gigante aqui dentro)
         "Leitura e Interpretação de Desenho Técnico": {
             "periodo": 1,
             "requisito": []
         },
-        // ... (resto das matérias de 2026) ...
+        "Cálculo I": {
+            "periodo": 1,
+            "requisito": []
+        },
+        "Química Geral": {
+            "periodo": 1,
+            "requisito": []
+        },
+        "Física I": {
+            "periodo": 2,
+            "requisito": ["Cálculo I"]
+        }
     }
 };
